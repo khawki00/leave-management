27903e11bd92bf03066d7791d88e07794808705b
khawki00 <khawki00@hotmail.com>
2020-03-21 09:27:44 -0400

    Add project files.

---
+++ leave-management/Data/LeaveHistory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Data
+{
+    public class LeaveHistory
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("RequestingEmployeeId")]
+        public string RequestingEmployee { get; set; }
+        public string RequestingEmployeeId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndTime { get; set; }
+
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public DateTime DateRequested { get; set; }
+        public DateTime DateActioned { get; set; }
+        public bool? Approved { get; set; }
+
+        [ForeignKey("ApprovedById")]
+        public Employee ApprovedBy { get; set; }
+        public string ApprovedById { get; set; }
+    }
+}
